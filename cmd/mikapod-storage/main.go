792ebbfcc196d32e84d6abff6aca52dc0dc1f72f
bartmika <bart@mikasoftware.com>
2019-09-01 12:13:03 -0400

    Simplified main.go

--- cmd/mikapod-storage/main.go
+++ cmd/mikapod-storage/main.go
@@ -3,25 +3,16 @@ package main // github.com/mikaponics/mikapod-soil/cmd/storage
 import (
 	"log"
 	"net"
-	"context"
 
 	"google.golang.org/grpc"
 
 	pb "github.com/mikaponics/mikapod-storage/api"
 	"github.com/mikaponics/mikapod-soil/configs"
 	"github.com/mikaponics/mikapod-storage/internal/storage"
+	"github.com/mikaponics/mikapod-storage/internal/rpc"
 )
 
-type server struct{}
 
-func (s *server) AddTimeSeriesDatum(ctx context.Context, in *pb.TimeSeriesDatumRequest) (*pb.MikapodStorageResponse, error) {
-	ms := storage.InitMikapodStorage()
-	ms.InsertTimeSeriesData(in.Instrument, in.Value, in.Timestamp)
-	return &pb.MikapodStorageResponse{
-		Message: "Instrument " + in.Instrument,
-		Status: true,
-	}, nil
-}
 
 func main() {
 	// Create our database on program load if it has not been created previously.
@@ -36,7 +27,7 @@ func main() {
 
 	// Initialize our gRPC server using our TCP server.
 	grpcServer := grpc.NewServer()
-	pb.RegisterMikapodStorageServer(grpcServer, &server{})
+	pb.RegisterMikapodStorageServer(grpcServer, &rpc.MikapodStorageGRPC{})
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
---
+++ internal/rpc/rpc.go
@@ -0,0 +1,19 @@
+package rpc
+
+import (
+	"context"
+
+	pb "github.com/mikaponics/mikapod-storage/api"
+	"github.com/mikaponics/mikapod-storage/internal/storage"
+)
+
+type MikapodStorageGRPC struct{}
+
+func (s *MikapodStorageGRPC) AddTimeSeriesDatum(ctx context.Context, in *pb.TimeSeriesDatumRequest) (*pb.MikapodStorageResponse, error) {
+	ms := storage.InitMikapodStorage()
+	ms.InsertTimeSeriesData(in.Instrument, in.Value, in.Timestamp)
+	return &pb.MikapodStorageResponse{
+		Message: "Instrument " + in.Instrument,
+		Status: true,
+	}, nil
+}
