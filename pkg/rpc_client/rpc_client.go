638e9937247cf9f1880c2656805cc69b5c4b9964
bartmika <bart@mikasoftware.com>
2021-01-07 23:01:54 -0500

    Added multiple attempt handling for DailHTTP.

--- pkg/rpc_client/rpc_client.go
+++ pkg/rpc_client/rpc_client.go
@@ -4,10 +4,31 @@ import (
 	// "errors"
 	"log"
 	"net/rpc"
+	"time"
 )
 
 type MikapodStorageService struct {
 	Client *rpc.Client
+}
+
+// Function will attempt to connect to the RPC server and if the attempt fails
+// then wait 10 seconds and retry - if 100 attempts fail then a fatal panic
+// will be triggered.
+func retryMultipleAndBlockingDailHTTP(addr string) (*rpc.Client) {
+	i := 0
+	for {
+		client, err := rpc.DialHTTP("tcp", addr)
+		if err != nil {
+			i = i + 1
+			if i >= 100 {
+				log.Fatal("ERROR | MikapodStorageService | retryMultipleAndBlockingDailHTTP | Dialing TCP Error:", err)
+			}
+			time.Sleep(10 * time.Second)
+		} else {
+			return client
+		}
+	}
+	return nil
 }
 
 func New(addr string) *MikapodStorageService {
@@ -15,9 +36,11 @@ func New(addr string) *MikapodStorageService {
 		log.Fatal("LOG | MikapodStorageService | New | No address set.")
 	}
 
+    // The following code will dial the server or attempt to redial the server
+	// 100 times before it the client returns a fatal error.
 	client, err := rpc.DialHTTP("tcp", addr)
 	if err != nil {
-		log.Fatal("ERROR | MikapodStorageService | New | Dialing TCP Error:", err)
+		client = retryMultipleAndBlockingDailHTTP(addr)
 	}
 
 	return &MikapodStorageService{
